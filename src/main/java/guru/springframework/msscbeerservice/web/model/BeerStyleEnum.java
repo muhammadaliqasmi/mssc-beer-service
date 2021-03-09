532b69bb7ed05949077f3b203d8fd952b3fb5e39
Muhammad Ali Qasmi <Ali.Qasmi@gfk.com>
2021-03-09 14:02:47 +0500

    - Adds web data model, closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by Qasmi on 2021-03-09
+ */
+public enum BeerStyleEnum {
+    LAGER, //
+    PILSNER, //
+    STOUT, //
+    GOSE, //
+    PORTER, //
+    ALE, //
+    WHEAT, //
+    IPA, //
+    PALE_ALE, //
+    SAISON
+}
